cb400050847faa7de0787e213fb02328c05935f2
“robmonday” <10903531+robmonday@users.noreply.github.com>
2023-02-23 13:01:39 -0500

    updates to backend while working on exercise 9.21

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import express from "express";
+import express from 'express';
 const app = express();
-import bodyParser from "body-parser";
-import cors from "cors";
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-import diagnoseRouter from "./routes/diagnoses";
-import patientRouter from "./routes/patients";
+import diagnoseRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/ping", (_req, res) => {
-  res.send("pong");
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
 });
 
-app.use("/api/diagnoses", diagnoseRouter);
-app.use("/api/patients", patientRouter);
+app.use('/api/diagnoses', diagnoseRouter);
+app.use('/api/patients', patientRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
